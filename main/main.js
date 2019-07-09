d4936b860b56700dd443708357cc12cea8c2036d
SCAUZSJ <895104160@qq.com>
2019-07-09 20:37:41 +0800

    submit.

--- main/main.js
+++ main/main.js
@@ -1,5 +1,30 @@
 'use strict';
 
 function printReceipt(inputs) {
-  console.log('请在此实现练习要求，并改写该行代码。');
+  let obj = objectConversionforMergeRepeat(inputs);
+  console.log(createReceipt(obj));
+}
+//用于合并的对象转换
+function objectConversionforMergeRepeat(inputs){
+  let obj = {};
+  inputs.forEach(item => {
+    obj[item] = item in obj ? ++obj[item] : 1;
+  });
+  return obj;
+}
+function createReceipt(obj){
+  let output = `***<没钱赚商店>收据***\n`;
+  let items = loadAllItems();
+  let total = 0;
+  for (var pro in obj) {
+    for (let i = 0; i < items.length; ++i) {
+      if (items[i].barcode == pro) {
+        total += obj[pro] * items[i].price;
+        output += `名称：${items[i].name}，数量：${obj[pro]}${items[i].unit}，单价：${items[i].price.toFixed(2)}(元)，小计：${(obj[pro] * items[i].price).toFixed(2)}(元)\n`
+        break;
+      }
+    }
+  }
+  output += `----------------------\n总计：${total.toFixed(2)}(元)\n**********************`
+  return output;
 }
